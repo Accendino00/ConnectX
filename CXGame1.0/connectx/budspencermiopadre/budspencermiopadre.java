62713a055b4dbbc79ac19fc98c85f8ad34eefc5f
Rinma Trupe <petrummarincas@gmail.com>
2023-07-18 13:00:56 +0200

    Prime modifiche

--- CXGame1.0/connectx/budspencermiopadre/budspencermiopadre.java
+++ CXGame1.0/connectx/budspencermiopadre/budspencermiopadre.java
@@ -1,9 +1,15 @@
 package connectx.budspencermiopadre;
 
 import connectx.CXPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import connectx.CXBoard;
 
 public class budspencermiopadre implements CXPlayer {
+
+
 
     @Override
     public void initPlayer(int M, int N, int X, boolean first, int timeout_in_secs) {
@@ -19,7 +25,18 @@ public class budspencermiopadre implements CXPlayer {
     public String playerName() {
         return "budspencermiopadre";
     }
+}
 
-    
+class TreeNode {
+    int value;
+    List<TreeNode> children;
 
+    public TreeNode(int value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild(TreeNode child) {
+        this.children.add(child);
+    }
 }
